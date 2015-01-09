b40f07126ebd1d12dcea4fad2629e06cee158136
northwolf0614 <jacklin@Jacks-MacBook-Pro.local>
2015-01-09 13:59:42 +1100

    use mask layer to cut make the animation

--- E1/E1/Views/ProgressBar.m
+++ E1/E1/Views/ProgressBar.m
@@ -7,7 +7,7 @@
 @interface ProgressBar()
 //@property(nonatomic,assign) CGFloat percent;
 
-//@property(nonatomic,strong) CAShapeLayer* backgroundLayer;
+@property(nonatomic,strong) CAShapeLayer* maskLayer;
 //@property(nonatomic,strong) CABasicAnimation* animation;
 //@property(nonatomic,strong) CABasicAnimation* animation1;
 @property(nonatomic,strong) UIColor* color1;
@@ -30,10 +30,10 @@
     [super layoutSubviews];
     self.animationLayer.frame =self.bounds;
     //setup the path
-    UIBezierPath *circlePath = [UIBezierPath bezierPath];
+    
     CGFloat radius= self.animationLayer.frame.size.width>self.animationLayer.frame.size.height? self.animationLayer.frame.size.height/2:self.animationLayer.frame.size.width/2;
     radius-=kcProgress_Line_Width/2;
-    [circlePath addArcWithCenter:CGPointMake(CGRectGetMidX(self.animationLayer.bounds), CGRectGetMidY(self.animationLayer.bounds)) radius: radius startAngle:0 endAngle:2*M_PI clockwise:YES];
+    
     //set up path1
     UIBezierPath *circlePath1 = [UIBezierPath bezierPath];
     float endPoint=0.0f;
@@ -51,27 +51,28 @@
     //set up path2
     UIBezierPath *circlePath2 = [UIBezierPath bezierPath];
     [circlePath2 addArcWithCenter:CGPointMake(CGRectGetMidX(self.animationLayer.bounds), CGRectGetMidY(self.animationLayer.bounds)) radius: radius startAngle:endPoint  endAngle:1.5*M_PI clockwise:YES];
+    
+    UIBezierPath *maskPath = [UIBezierPath bezierPath];
+    [maskPath addArcWithCenter:CGPointMake(CGRectGetMidX(self.animationLayer.bounds), CGRectGetMidY(self.animationLayer.bounds)) radius: radius startAngle:1.5*M_PI endAngle:3.5*M_PI clockwise:YES];
 
-    //setup the background layer
-//    self.backgroundLayer.frame=self.animationLayer.bounds;
-//    self.backgroundLayer.path = circlePath.CGPath;
-//    self.backgroundLayer.strokeColor = [[UIColor lightGrayColor] CGColor];
-//    self.backgroundLayer.fillColor = [[UIColor clearColor] CGColor];
-//    self.backgroundLayer.lineWidth = kcTrack_LineE_Width;
-    //setup the progress layer
     self.progressLayer.frame=self.animationLayer.bounds;
     self.progressLayer.strokeColor = [self.color1 CGColor];
     self.progressLayer.fillColor = [[UIColor clearColor] CGColor];
     self.progressLayer.lineWidth=kcProgress_Line_Width;
-    self.progressLayer.strokeEnd=0.f;//use this property to set up the default value
     self.progressLayer.path=circlePath1.CGPath;
 //
     self.progressLayerPlus.frame=self.animationLayer.bounds;
     self.progressLayerPlus.strokeColor = [self.color2 CGColor];
     self.progressLayerPlus.fillColor = [[UIColor clearColor] CGColor];
     self.progressLayerPlus.lineWidth=kcProgress_Line_Width;
-    self.progressLayerPlus.strokeEnd=0.f;//use this property to set up the default value
     self.progressLayerPlus.path=circlePath2.CGPath;
+    
+    self.maskLayer.frame=self.animationLayer.bounds;
+    self.maskLayer.strokeColor = [self.color2 CGColor];
+    self.maskLayer.fillColor = [[UIColor clearColor] CGColor];
+    self.maskLayer.lineWidth=kcProgress_Line_Width;
+    self.maskLayer.strokeEnd=0.f;//use this property to set up the default value
+    self.maskLayer.path=maskPath.CGPath;
 }
 - (id)initWithFinalPercentage:(CGFloat)percentage color1:(UIColor*)color1 color2:(UIColor*)color2
 {
@@ -82,8 +83,10 @@
         [self.layer addSublayer:self.animationLayer];
         self.progressLayer = [CAShapeLayer layer];
         self.progressLayerPlus = [CAShapeLayer layer];
+        self.maskLayer=[CAShapeLayer layer];
         [self.animationLayer addSublayer:self.progressLayer];
         [self.animationLayer addSublayer:self.progressLayerPlus];
+        self.animationLayer.mask=self.maskLayer;
         self.color1=color1;
         self.color2=color2;
         self.finalPercentage=percentage;
@@ -108,13 +111,12 @@
         [animation setFromValue:[NSNumber numberWithFloat:0.0f] ];
         //[animation setFromValue:0 ];
         [animation setToValue:[NSNumber numberWithFloat:1.0f]];
-        [animation setDuration:kcAnimationTime/2];
+        [animation setDuration:kcAnimationTime];
         [animation setRemovedOnCompletion:NO];
         [animation setFillMode:kCAFillModeForwards];//must set up this property, otherwise this class does not work properly
         [animation setTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear]];
-        animation.delegate=self;
         [animation setValue:@"ProgressBarAnimation" forKey:@"animationBar"];
-        [[self progressLayer] addAnimation:animation forKey:nil];
+        [self.maskLayer addAnimation:animation forKey:nil];
     }
     
     
@@ -123,7 +125,7 @@
 -(void)deleteAnimatedProgress
 {
 
-    [self.progressLayer removeAllAnimations];
+    [self.maskLayer removeAllAnimations];
     [self.animationLayer removeFromSuperlayer];//delete the layer, then the layer will disappear
 
 }
@@ -144,21 +146,21 @@
 #pragma CAAnimationDelegate
 - (void)animationDidStop:(CAAnimation *)animation finished:(BOOL)flag
 {
-    if ([[animation valueForKey:@"animationBar"] isEqualToString:@"ProgressBarAnimation"])
-    {
-        
-        CABasicAnimation *animation1;
-        animation1=[CABasicAnimation animationWithKeyPath:@"strokeEnd"];//this place the input parameter must be strokeEnd
-        //[animation1 setFromValue:[NSNumber numberWithFloat:self.percent] ];
-        [animation1 setFromValue:[NSNumber numberWithDouble:0.0f] ];
-        [animation1 setToValue:[NSNumber numberWithDouble:1.0f]];
-        [animation1 setDuration:kcAnimationTime/2];
-        [animation1 setRemovedOnCompletion:NO];
-        [animation1 setFillMode:kCAFillModeForwards];//must set up this property, otherwise this class does not work properly
-        [animation1 setTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear]];
-        [[self progressLayerPlus] addAnimation:animation1 forKey:nil];
-
-    }
+//    if ([[animation valueForKey:@"animationBar"] isEqualToString:@"ProgressBarAnimation"])
+//    {
+//        
+//        CABasicAnimation *animation1;
+//        animation1=[CABasicAnimation animationWithKeyPath:@"strokeEnd"];//this place the input parameter must be strokeEnd
+//        //[animation1 setFromValue:[NSNumber numberWithFloat:self.percent] ];
+//        [animation1 setFromValue:[NSNumber numberWithDouble:0.0f] ];
+//        [animation1 setToValue:[NSNumber numberWithDouble:1.0f]];
+//        [animation1 setDuration:kcAnimationTime/2];
+//        [animation1 setRemovedOnCompletion:NO];
+//        [animation1 setFillMode:kCAFillModeForwards];//must set up this property, otherwise this class does not work properly
+//        [animation1 setTimingFunction:[CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionLinear]];
+//        [[self progressLayerPlus] addAnimation:animation1 forKey:nil];
+//
+//    }
     
     
     
